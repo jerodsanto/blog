475a2235407f46cd8944324f1fcff019010e3c92
Jerod Santo <jerod.santo@gmail.com>
2026-02-24 14:38:38 -0600

    Changed my mind

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ cd ~/src/jerodsanto/net
 echo "Building site with Hugo..."
 hugo --quiet
 
-echo "Deploying to mydh:~/jerodsanto.net..."
-rsync -az public/ mydh:~/jerodsanto.net/
+echo "Deploying to dh:~/jerodsanto.net..."
+rsync -az public/ dh:~/jerodsanto.net/
 
 echo "Deployment complete!"
